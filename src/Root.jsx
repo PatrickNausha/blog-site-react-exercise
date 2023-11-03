2b4ad3991f45303e7ec5f850b15c1ea5701f1abe
Patrick Nausha <patrick.nausha@flexe.com>
2023-11-03 13:50:10 -0700

    Start adding basic styling.

--- src/Root.jsx
+++ src/Root.jsx
@@ -5,7 +5,24 @@ export function Root() {
   const { currentUser } = useAuthentication();
   return (
     <div>
-      <h1>The Best Blog Site Ever</h1>
+      <div class="container py-5 my-4">
+        <h1 class="text-center mb-4">Ultimate Blog Site</h1>
+        <p class="text-center mb-4">
+          This is a coding exercise blog site. Do not take the content
+          seriously.
+        </p>
+        <div class="d-flex">
+          <span class="mx-auto">
+            <a
+              href="https://github.com/patricknausha/threejs-demos"
+              target="_blank"
+              rel="noreferrer"
+            >
+              View source
+            </a>
+          </span>
+        </div>
+      </div>
       {
         // TODO: hide on sign-in page
         !currentUser && <a href="/sign-in">Sign in</a>
