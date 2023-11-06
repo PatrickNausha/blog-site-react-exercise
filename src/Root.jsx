8c570bb7ab36d6516e3c7639cc20521c5991b184
Patrick Nausha <patrick.nausha@flexe.com>
2023-11-06 15:08:26 -0800

    Offer navigation path back to homepage.

--- src/Root.jsx
+++ src/Root.jsx
@@ -6,7 +6,11 @@ export function Root() {
   return (
     <div>
       <div className="container py-5 my-4">
-        <h1 className="text-center mb-4">Ultimate Blog Site</h1>
+        <h1 className="text-center mb-4">
+          <Link className="text-decoration-none" href="/">
+            Ultimate Blog Site
+          </Link>
+        </h1>
         <p className="text-center mb-4">
           This is a coding exercise blog site. Do not take the content
           seriously.
